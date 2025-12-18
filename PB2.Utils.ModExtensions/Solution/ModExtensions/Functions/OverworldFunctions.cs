be080a52762a5c97dea144756f02e32c9e6ea6d3
Artyom <artyom@braceyourselfgames.com>
2025-12-18 08:30:06 -0800

    Minor restructuring. Testing addition of an overworld function.

---
+++ PB2.Utils.ModExtensions/Solution/ModExtensions/Functions/OverworldFunctions.cs
@@ -0,0 +1,18 @@
+﻿using PhantomBrigade.Functions;
+using UnityEngine;
+
+namespace ModExtensions.Functions
+{
+    public class OverworldFunctions
+    {
+        public class MyCustomFunction : IOverworldFunction
+        {
+            public string input;
+
+            public void Run ()
+            {
+                Debug.Log ($"Running custom function from ModExtensions: {input}");
+            }
+        }
+    }
+}
